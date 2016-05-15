f3c86e480aa2182b725d941cd951986218d8428a
nazmul <nazmul.apu.hasan@gmail.com>
2016-05-15 18:44:06 +0300

    Revert "Revert "apache wicket ajax form""
    This reverts commit d7c0f3129d5e79b60a70259eb7ffdb2be4dadc1f.

---
+++ homework9/src/main/java/com/nazmulhasanapu/model/USerModel.java
@@ -0,0 +1,25 @@
+package com.nazmulhasanapu.model;
+
+public class USerModel {
+	
+	private String name;
+	private String gender;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getGender(){
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+ 
+}
